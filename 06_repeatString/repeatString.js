8f4fe37c08d819c69460eba6982bb01c75d26600
Nandan2006-oss <185660510+Nandan2006-oss@users.noreply.github.com.>
2025-10-26 14:44:04 +0530

    .

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(oldString, num) {
+    if (num >= 0) {
+        newString = "";
+        for(i = 0; i < num; i++) {
+            newString += oldString;
+        }
+        return newString;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    newArray = [];
+    newArray = array.filter(item => !(args.includes(item)));
+    return newArray;
 };
 
 // Do not edit below this line
